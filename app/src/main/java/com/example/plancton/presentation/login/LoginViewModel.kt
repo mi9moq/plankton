734471d4eb4f8197a8b75cae9344c75f888de028
Ельмиров Даниил Владимирович <elmirovwork@gmail.com>
2023-07-07 22:23:23 +0700

    Иммитация регистрации и авторизации.

--- app/src/main/java/com/example/plancton/presentation/login/LoginViewModel.kt
+++ app/src/main/java/com/example/plancton/presentation/login/LoginViewModel.kt
@@ -10,11 +10,13 @@ import com.example.plancton.domain.entity.AuthErrorType.HTTP400
 import com.example.plancton.domain.entity.AuthErrorType.HTTP401
 import com.example.plancton.domain.entity.AuthErrorType.INTERNET
 import com.example.plancton.domain.entity.AuthErrorType.UNKNOWN
+import com.example.plancton.domain.usecase.GetTokenUseCase
 import com.example.plancton.domain.usecase.LoginUseCase
 import com.example.plancton.presentation.login.LoginState.Error
 import com.example.plancton.presentation.login.LoginState.Initial
 import com.example.plancton.presentation.login.LoginState.Loading
 import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import retrofit2.HttpException
 import java.net.ConnectException
@@ -25,6 +27,7 @@ import javax.inject.Inject
 
 class LoginViewModel @Inject constructor(
     private val loginUseCase: LoginUseCase,
+    private val getTokenUseCase: GetTokenUseCase,
 ) : ViewModel() {
 
     private val _state = MutableLiveData<LoginState>(Initial)
@@ -52,17 +55,26 @@ class LoginViewModel @Inject constructor(
         }
     }
 
+    //TODO иммитация авторизации; переделать
     fun login(auth: Auth) {
         _state.value = Loading
 
+        val emailRegex = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$".toRegex()
+        val token = getTokenUseCase()
+
         viewModelScope.launch(handleError) {
             //TODO переделать при появлении api
-            //delay(1000)
-            Log.d("TAG", loginUseCase(auth))
-            //throw ConnectException()
-            //throw HttpException(Response.error<Any>(400, "s".toResponseBody()))
-            //throw HttpException(Response.error<Any>(401, "s".toResponseBody()))
-            //throw NullPointerException()
+            delay(1000)
+
+            if (auth.email.contains(emailRegex) && auth.password.isNotBlank()) {
+
+                if (auth.email + auth.password != token)
+                    _state.value = Error(HTTP401)
+                else {
+                    Log.d("LOGIN", loginUseCase(auth))
+                }
+            } else
+                _state.value = Error(HTTP400)
         }
     }
 }
