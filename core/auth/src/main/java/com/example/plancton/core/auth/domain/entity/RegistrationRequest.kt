8267c499b0fbc3c533610d3762494eb4f7d7aab9
Миронов Денис Владимирович <denis0799_97@mail.ru>
2023-07-10 16:41:19 +0700

    Перенес domain слой

--- core/auth/src/main/java/com/example/plancton/core/auth/domain/entity/RegistrationRequest.kt
+++ core/auth/src/main/java/com/example/plancton/core/auth/domain/entity/RegistrationRequest.kt
@@ -1,4 +1,4 @@
-package com.example.plancton.domain.entity
+package com.example.plancton.core.auth.domain.entity
 
 data class RegistrationRequest(
     val email: String,
---
+++ core/auth/src/main/java/com/example/plancton/core/auth/domain/repository/AuthRepository.kt
@@ -0,0 +1,11 @@
+package com.example.plancton.core.auth.domain.repository
+
+import com.example.plancton.core.auth.domain.entity.Auth
+import com.example.plancton.core.auth.domain.entity.RegistrationRequest
+
+interface AuthRepository {
+
+    suspend fun login(auth: Auth): String
+
+    suspend fun register(registrationRequest: RegistrationRequest)
+}
