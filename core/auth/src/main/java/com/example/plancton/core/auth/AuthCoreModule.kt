eab8401fd56715d67061b62453a1d304628ae457
Ельмиров Даниил Владимирович <elmirovwork@gmail.com>
2023-07-12 22:35:58 +0700

    Убрал неиспользуемый код.

--- core/auth/src/main/java/com/example/plancton/core/auth/AuthCoreModule.kt
+++ core/auth/src/main/java/com/example/plancton/core/auth/AuthCoreModule.kt
@@ -9,10 +9,7 @@ import com.example.plancton.core.dagger.AppScope
 import dagger.Binds
 import dagger.Module
 import dagger.Provides
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.create
 
 @Module
@@ -27,26 +24,6 @@ interface AuthCoreModule {
     fun bindAuthRepository(impl: AuthRepositoryImpl): AuthRepository
 
     companion object {
-
-//        private const val BASE_URL = "http://81.177.197.88:8080/"
-//
-//        @AppScope
-//        @Provides
-//        fun provideHttpClient(): OkHttpClient =
-//            OkHttpClient.Builder()
-//                .addInterceptor(HttpLoggingInterceptor().apply {
-//                    level = HttpLoggingInterceptor.Level.BODY
-//                })
-//                .build()
-//
-//        @AppScope
-//        @Provides
-//        fun provideRetrofit(httpClient: OkHttpClient): Retrofit =
-//            Retrofit.Builder()
-//                .baseUrl(BASE_URL)
-//                .client(httpClient)
-//                .addConverterFactory(GsonConverterFactory.create())
-//                .build()
 
         @AppScope
         @Provides
