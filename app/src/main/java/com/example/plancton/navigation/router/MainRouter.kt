158e722be94fee7c103283a9c5e51b75893cb110
Ельмиров Даниил Владимирович <elmirovwork@gmail.com>
2023-07-13 20:28:48 +0700

    Добавил навигацию с главного экрана на начальный.

--- app/src/main/java/com/example/plancton/navigation/router/MainRouter.kt
+++ app/src/main/java/com/example/plancton/navigation/router/MainRouter.kt
@@ -1,19 +1,26 @@
 package com.example.plancton.navigation.router
 
 import com.example.plancton.navigation.screen.getAddEventScreen
+import com.example.plancton.navigation.screen.getEntryFragment
 import com.github.terrakok.cicerone.Router
 import javax.inject.Inject
 
 interface MainRouter {
 
     fun openAddEvent()
+
+    fun openEntry()
 }
 
 class MainRouterImpl @Inject constructor(
-    private val router: Router
+    private val router: Router,
 ) : MainRouter {
 
     override fun openAddEvent() {
         router.navigateTo(getAddEventScreen())
+    }
+
+    override fun openEntry() {
+        router.newRootScreen(getEntryFragment())
     }
 }
