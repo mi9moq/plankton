52791638d2eee001c2eefea7fe5e6ee1fa3bff53
Миронов Денис Владимирович <denis0799_97@mail.ru>
2023-07-13 22:49:38 +0700

    Удаление инвента с сервера

--- core/event/src/main/java/com/examlpe/plancton/core/event/data/network/api/EventApi.kt
+++ core/event/src/main/java/com/examlpe/plancton/core/event/data/network/api/EventApi.kt
@@ -3,8 +3,10 @@ package com.examlpe.plancton.core.event.data.network.api
 import com.examlpe.plancton.core.event.data.network.model.UserEventDto
 import com.examlpe.plancton.core.event.domain.entity.EventRequest
 import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Path
 import retrofit2.http.Query
 import java.time.LocalDate
 
@@ -19,4 +21,7 @@ interface EventApi {
     suspend fun getAll(
         @Query("start_date") startDate: LocalDate, @Query("end_date") endDate: LocalDate,
     ): List<UserEventDto>
+
+    @DELETE("/api/event/{event_id}")
+    suspend fun delete(@Path("event_id") id: String)
 }
